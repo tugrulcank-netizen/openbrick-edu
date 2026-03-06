1e4fdd5cf808a175203018bc465f301e3360432a
tugrul kapubagli <tugrulcank@gmail.com>
2026-03-07 00:08:50 +0300

    fix(ide): add jsdom test environment and TextEncoder polyfill for CI

--- ide/jest.config.js
+++ ide/jest.config.js
@@ -1,6 +1,7 @@
 export default {
   preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'node',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFiles: ['./jest.setup.ts'],
   testMatch: ['**/src/**/__tests__/**/*.test.ts'],
   transform: {
     '^.+\\.ts$': ['ts-jest', { useESM: true, diagnostics: false, tsconfig: 'tsconfig.jest.json' }],
